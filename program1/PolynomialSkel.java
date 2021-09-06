39bb2bc12758d3e0d53207951afb4adccb6277b9
Expitau <jaeyunkim@snu.ac.kr>
2021-09-06 04:44:10 +0000

    make program1

---
+++ program1/PolynomialSkel.java
@@ -0,0 +1,20 @@
+// Skeleton of the Polynomial ADT
+
+public class Polynomial {
+
+  // Create an empty polynomial
+  public Polynomial() {}
+
+  // Create a single-term polynomial
+  public Polynomial(int coef, int exp) {}
+
+  // Add opnd to 'this' polynomial; 'this' is returned
+  public Polynomial add(Polynomial opnd) {}
+
+  // Subtract opnd from 'this' polynomial; 'this' is returned
+  public Polynomial sub(Polynomial opnd) {}
+
+  // Print the terms of 'this' polynomial in decreasing order of exponents.
+  // No pair of terms can share the same exponent in the printout.
+  public void print() {}
+}
