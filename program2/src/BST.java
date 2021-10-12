ffc765fa9b3c302abf2ed6f4169828ec071ab926
Jaeyun Kim <ktwin01@icloud.com>
2021-10-12 21:35:01 +0900

    program2 reformating

--- program2/src/BST.java
+++ program2/src/BST.java
@@ -3,93 +3,108 @@
 
 public class BST { // Binary Search Tree implementation
 
-  protected boolean NOBSTified = false;
-  protected boolean OBSTified = false;
-  protected Node root;
-  protected int totSize, totFreq;
+    protected boolean NOBSTified = false;
+    protected boolean OBSTified = false;
+    protected Node root;
+    protected int totSize, totFreq;
 
-  public BST() {
-    root = null;
-    totSize = 0;
-    totFreq = 0;
-  }
-
-  // Tree node operation
-  protected Node makeNewNode(String key){
-    return new Node(key);
-  }
-  protected void increaseFrequency(Node node){
-    node.increaseFrequency();
-  }
-
-  // return nextNode
-  // if there are no nextNode, make node or increase frequency
-  protected Node getNextNode(Node node, String key){
-    int flag = key.compareTo(node.getKey());
-    if(flag < 0){
-      if(node.left == null){
-        node.left = makeNewNode(key);
-        return null;
-      }
-      return node.left;
-    }else if(flag > 0){
-      if(node.right == null){
-        node.right = makeNewNode(key);
-        return null;
-      }
-      return node.right;
-    }else{
-      increaseFrequency(node);
-      return null;
+    public BST() {
+        root = null;
+        totSize = 0;
+        totFreq = 0;
     }
-  }
 
-  //inserting key
-  public void insert(String key) {
-    System.out.println(key);
-    // check root is null
-    if(root == null){
-      root = makeNewNode(key);
-      return;
+    // Tree node operation
+    protected Node makeNewNode(String key) {
+        return new Node(key);
     }
-    
-    // find insert place
-    Node node = root;
-    while(node != null){
-      node = getNextNode(node, key);
+
+    protected void increaseFrequency(Node node) {
+        node.increaseFrequency();
     }
-  }
 
-  //find key
-  public boolean find(String key) {
-    Node node=root;
-    while(node != null){
-      
-      int flag = key.compareTo(node.key);
-      //System.out.println("find "+key+" : "+node.key + "    " + flag);
-      if(flag < 0) node = node.left;
-      else if(flag > 0) node = node.right;
-      else return true;
+    // return nextNode
+    // if there are no nextNode, make node or increase frequency
+    protected Node getNextNode(Node node, String key) {
+        int flag = key.compareTo(node.getKey());
+        if (flag < 0) {
+            if (node.left == null) {
+                node.left = makeNewNode(key);
+                return null;
+            }
+            return node.left;
+        } else if (flag > 0) {
+            if (node.right == null) {
+                node.right = makeNewNode(key);
+                return null;
+            }
+            return node.right;
+        } else {
+            increaseFrequency(node);
+            return null;
+        }
     }
-    return false;
-  }
 
+    // inserting key
+    public void insert(String key) {
+        System.out.println(key);
+        // check root is null
+        if (root == null) {
+            root = makeNewNode(key);
+            return;
+        }
 
-  public int size() {
-    return totSize;
-  }
+        // find insert place
+        Node node = root;
+        while (node != null) {
+            node = getNextNode(node, key);
+        }
+    }
 
-  public int sumFreq() {
-    return totFreq;
-  }
-  public int sumProbes() { return 1;}
-  public int sumWeightedPath() { return 1; }
-  public void resetCounters() { }
+    // find key
+    public boolean find(String key) {
+        Node node = root;
+        while (node != null) {
 
-  public void nobst() { }	// Set NOBSTified to true.
-  public void obst() { }	// Set OBSTified to true.
-  public void print() {
+            int flag = key.compareTo(node.key);
+            // System.out.println("find "+key+" : "+node.key + " " + flag);
+            if (flag < 0)
+                node = node.left;
+            else if (flag > 0)
+                node = node.right;
+            else
+                return true;
+        }
+        return false;
+    }
 
-  }
+    public int size() {
+        return totSize;
+    }
+
+    public int sumFreq() {
+        return totFreq;
+    }
+
+    public int sumProbes() {
+        return 1;
+    }
+
+    public int sumWeightedPath() {
+        return 1;
+    }
+
+    public void resetCounters() {
+    }
+
+    public void nobst() {
+    } // Set NOBSTified to true.
+
+    public void obst() {
+    } // Set OBSTified to true.
+
+    public void print() {
+
+    }
 
 }
--- program2/src/Node.java
+++ program2/src/Node.java
@@ -1,58 +1,58 @@
 class Node {
-  public String key;
-  public int frequency, accessCnt;
-  public Node left, right;
-  public int h, bf;
+    public String key;
+    public int frequency, accessCnt;
+    public Node left, right;
+    public int h, bf;
 
-  public Node(String key) {
-    this.key = key;
-    frequency = 1;
-    accessCnt = 0;
-    left = null;
-    right = null;
-    h = 0;
-    bf = 0;
-  }
+    public Node(String key) {
+        this.key = key;
+        frequency = 1;
+        accessCnt = 0;
+        left = null;
+        right = null;
+        h = 0;
+        bf = 0;
+    }
 
-  public static Node clone(Node node) {
-    if (node == null)
-      return node;
-    Node ret = new Node("");
-    ret.assignNode(node);
-    return ret;
-  }
+    public static Node clone(Node node) {
+        if (node == null)
+            return node;
+        Node ret = new Node("");
+        ret.assignNode(node);
+        return ret;
+    }
 
-  public void assignNode(Node node) {
-    this.key = node.key;
-    this.frequency = node.frequency;
-    this.accessCnt = node.accessCnt;
-    this.left = node.left;
-    this.right = node.right;
-    this.h = node.h;
-    this.bf = node.bf;
-  }
+    public void assignNode(Node node) {
+        this.key = node.key;
+        this.frequency = node.frequency;
+        this.accessCnt = node.accessCnt;
+        this.left = node.left;
+        this.right = node.right;
+        this.h = node.h;
+        this.bf = node.bf;
+    }
 
-  public int getFrequency() {
-    return frequency;
-  }
+    public int getFrequency() {
+        return frequency;
+    }
 
-  public String getKey() {
-    accessCnt++;
-    return key;
-  }
+    public String getKey() {
+        accessCnt++;
+        return key;
+    }
 
-  public void increaseFrequency() {
-    frequency++;
-  }
+    public void increaseFrequency() {
+        frequency++;
+    }
 
-  public void refreshHeight() {
-    int lh = 0, rh = 0;
-    if (left != null)
-      lh = left.h;
-    if (right != null)
-      rh = right.h;
-    h = 1 + Math.max(lh, rh);
-    bf = lh - rh;
-  }
+    public void refreshHeight() {
+        int lh = 0, rh = 0;
+        if (left != null)
+            lh = left.h;
+        if (right != null)
+            rh = right.h;
+        h = 1 + Math.max(lh, rh);
+        bf = lh - rh;
+    }
 
 }
