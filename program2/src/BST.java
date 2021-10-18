7556cf03f57cbc478237176700d97049a0bddc5a
Jaeyun Kim <ktwin01@icloud.com>
2021-10-18 12:15:53 +0900

    add all detalis of BST/AVL

--- program2/src/BST.java
+++ program2/src/BST.java
@@ -1,95 +1,266 @@
+import java.util.Queue;
+import java.util.Stack;
+import java.util.LinkedList;
 // (Nearly) Optimal Binary Search Tree
 // Bongki Moon (bkmoon@snu.ac.kr)
 
 public class BST { // Binary Search Tree implementation
 
-  protected boolean NOBSTified = false;
-  protected boolean OBSTified = false;
-  protected Node root;
-  protected int totSize, totFreq;
+    protected boolean NOBSTified = false;
+    protected boolean OBSTified = false;
+    protected Node root;
+    protected int totSize, totFreq, totProbe;
 
-  public BST() {
-    root = null;
-    totSize = 0;
-    totFreq = 0;
-  }
+    public BST() {
+        root = null;
+        totSize = 0;
+        totFreq = 0;
+    }
 
-  // Tree node operation
-  protected Node makeNewNode(String key){
-    return new Node(key);
-  }
-  protected void increaseFrequency(Node node){
-    node.increaseFrequency();
-  }
+    // Tree node operation
+    protected Node makeNewNode(String key) {
+        totSize += 1;
+        totFreq += 1;
+        return new Node(key);
+    }
 
-  // return nextNode
-  // if there are no nextNode, make node or increase frequency
-  protected Node getNextNode(Node node, String key){
-    int flag = key.compareTo(node.getKey());
-    if(flag < 0){
-      if(node.left == null){
-        node.left = makeNewNode(key);
-        return null;
+    protected void increaseFrequency(Node node) {
+        totFreq += 1;
+        node.frequency += 1;
+    }
+
+    protected void increaseProbe(Node node){
+        totProbe += 1;
+        node.probe += 1;
+    }
+
+    // return nextNode
+    // if there are no nextNode, make node or increase frequency
+    protected Node getNextNode(Node node, String key) {
+        int flag = key.compareTo(node.key);
+        if (flag < 0) {
+            if (node.left == null) {
+                node.left = makeNewNode(key);
+                return null;
+            }
+            return node.left;
+        } else if (flag > 0) {
+            if (node.right == null) {
+                node.right = makeNewNode(key);
+                return null;
+            }
+            return node.right;
+        } else {
+            increaseFrequency(node);
+            return null;
+        }
+    }
+
+    // inserting key
+    public void insert(String key) {
+        if (root == null) {
+            root = makeNewNode(key);
+            return;
+        }
+
+        Node node = root;
+        Stack<Node> stk = new Stack<>();
+        while (node != null) {
+            stk.push(node);
+            node = getNextNode(node, key);
+        }
+        resolveStack(stk);
+    }
+
+    private void resolveStack(Stack<Node> stk){
+        Node node;
+        while(!stk.empty()){
+          node = stk.peek(); stk.pop();
+          node.refreshHeight();;
+        }
       }
-      return node.left;
-    }else if(flag > 0){
-      if(node.right == null){
-        node.right = makeNewNode(key);
-        return null;
-      }
-      return node.right;
-    }else{
-      increaseFrequency(node);
-      return null;
+
+    // find key
+    public boolean find(String key) {
+        Node node = root;
+        while (node != null) {
+            increaseProbe(node);
+            int flag = key.compareTo(node.key);
+            if (flag < 0)
+                node = node.left;
+            else if (flag > 0)
+                node = node.right;
+            else
+                return true;
+        }
+        return false;
     }
-  }
 
-  //inserting key
-  public void insert(String key) {
-    System.out.println(key);
-    // check root is null
-    if(root == null){
-      root = makeNewNode(key);
-      return;
+    public int size() {
+        return totSize;
     }
-    
-    // find insert place
-    Node node = root;
-    while(node != null){
-      node = getNextNode(node, key);
+
+    public int sumFreq() {
+        return totFreq;
     }
-  }
 
-  //find key
-  public boolean find(String key) {
-    Node node=root;
-    while(node != null){
-      
-      int flag = key.compareTo(node.key);
-      //System.out.println("find "+key+" : "+node.key + "    " + flag);
-      if(flag < 0) node = node.left;
-      else if(flag > 0) node = node.right;
-      else return true;
+    public int sumProbes() {
+        return totProbe;
     }
-    return false;
-  }
+
+    public int sumWeightedPath() {
+        Queue<Node> que = new LinkedList<>();
+        Queue<Integer> depthQue = new LinkedList<>();
+        que.add(root);
+        depthQue.add(1);
+        int ret = 0;
+        while(!que.isEmpty()){
+            Node node = que.poll(); 
+            int depth = depthQue.poll();
+            ret += depth * node.frequency;
+            if(node.left != null){
+                que.add(node.left);
+                depthQue.add(depth+1);
+            }
+            if(node.right != null){
+                que.add(node.right);
+                depthQue.add(depth+1);
+            }
+        }
+        return ret;
+    }
+
+    public void resetCounters() {
+        totFreq = 0;
+        totProbe = 0;
+        Queue<Node> que = new LinkedList<>();
+        que.add(root);
+        while(!que.isEmpty()){
+            Node node = que.poll(); 
+            node.frequency = 0;
+            node.probe = 0;
+            if(node.left != null) que.add(node.left);
+            if(node.right != null) que.add(node.right);
+        }
+    }
+
+    private static Node[] getNodesList(BST bst) {
+        Node[] nodes = new Node[bst.size() + 1];
+        fillNodesList(bst.root, nodes, 1);
+        return nodes;
+    }
+
+    private static int fillNodesList(Node node, Node[] nodes, int index) {
+        if (node == null)
+            return index;
+        index = fillNodesList(node.left, nodes, index);
+        nodes[index++] = node;
+        index = fillNodesList(node.right, nodes, index);
+        return index;
+    }
+
+    private static int[] getWeightSum(Node[] nodes) {
+        int sz = nodes.length;
+        int[] weightSum = new int[sz];
+        for (int i = 1; i < sz; i++) {
+            weightSum[i] = weightSum[i - 1] + nodes[i].frequency;
+        }
+        return weightSum;
+    }
+
+    public void nobst() {
+        NOBSTified = true;
+        Node[] nodes = getNodesList(this);
+        int[] weightSum = getWeightSum(nodes);
+        root = nobst(weightSum, nodes, 1, totSize);
+    } // Set NOBSTified to true.
+
+    private static Node nobst(int[] weightSum, Node[] nodes, int l, int r) {
+        if (l > r)
+            return null;
+
+        int root = l, lastMin = weightSum[r] - weightSum[l];
+        for (int i = l + 1; i <= r; i++) { // TODO.change to tenery search!!
+            int difference = Math.abs((weightSum[i - 1] - weightSum[l - 1]) - (weightSum[r] - weightSum[i]));
+            if (difference < lastMin) { // until right is heavier
+                root = i;
+                lastMin = difference;
+            }
+        }
+        Node ret = new Node("");
+        ret.assignNode(nodes[root]);
+        ret.left = nobst(weightSum, nodes, l, root - 1);
+        ret.right = nobst(weightSum, nodes, root + 1, r);
+        ret.refreshHeight();
+        return ret;
+    }
+
+    public void obst() {
+        OBSTified = true;
+        Node[] nodes = getNodesList(this);
+        int[] weightSum = getWeightSum(nodes);
+        int[][] rootTable = getRootTable(this, nodes, weightSum);
+        root = obst(nodes, rootTable, 1, totSize);
+        //
+    }
+
+    private static int[][] getRootTable(BST bst, Node[] nodes, int[] weightSum) { // TODO. reduce time!!!!!!!!!
+        int n = bst.size();
+        int[][] costTable = new int[n + 2][n + 1];
+        int[][] rootTable = new int[n + 2][n + 1];
+
+        for (int i = 1; i <= n; i++) { // j == i
+            rootTable[i][i] = i;
+            costTable[i][i] = weightSum[i] - weightSum[i - 1];
+        }
+        for (int i = 1; i < n; i++) { // j == i+1
+            if (costTable[i][i] >= costTable[i + 1][i + 1]) {
+                rootTable[i][i + 1] = i;
+                costTable[i][i + 1] = costTable[i + 1][i + 1] + weightSum[i + 1] - weightSum[i - 1];
+            } else {
+                rootTable[i][i + 1] = i + 1;
+                costTable[i][i + 1] = costTable[i][i] + weightSum[i + 1] - weightSum[i - 1];
+            }
+        }
+
+        for (int k = 2; k < n; k++) {
+            for (int i = 1; i + k <= n; i++) {
+                int j = i + k;
+                int root = rootTable[i][j - 1];
+                int minCost = costTable[i][root - 1] + costTable[root + 1][j];
+
+                for (int r = rootTable[i][j - 1]; r <= rootTable[i + 1][j]; r++) {
+                    if (costTable[i][r - 1] + costTable[r + 1][j] < minCost) {
+                        minCost = costTable[i][r - 1] + costTable[r + 1][j];
+                        root = r;
+                    }
+                }
+
+                rootTable[i][j] = root;
+                costTable[i][j] = minCost + weightSum[j] - weightSum[i - 1];
+            }
+        }
+        return rootTable;
+    }
+
+    private static Node obst(Node[] nodes, int[][] rootTable, int l, int r) {
+        if (l > r)
+            return null;
+        Node ret = new Node("");
+        int root = rootTable[l][r];
+        ret.assignNode(nodes[root]);
+        ret.left = obst(nodes, rootTable, l, root - 1);
+        ret.right = obst(nodes, rootTable, root + 1, r);
+        ret.refreshHeight();
+        return ret;
+    }
+    public void print() {
+        Node[] nodes = getNodesList(this);
+        for(int i=1; i<=totSize; i++){
+            System.out.println(nodes[i]);    
+        }
+    }
 
 
-  public int size() {
-    return totSize;
-  }
-
-  public int sumFreq() {
-    return totFreq;
-  }
-  public int sumProbes() { return 1;}
-  public int sumWeightedPath() { return 1; }
-  public void resetCounters() { }
-
-  public void nobst() { }	// Set NOBSTified to true.
-  public void obst() { }	// Set OBSTified to true.
-  public void print() {
-
-  }
 
 }
--- program2/src/TestBst.java
+++ program2/src/TestBst.java
@@ -3,6 +3,7 @@ import java.lang.management.*;
 
 class TestBst {
     public static ThreadMXBean TMB;
+
     public static void main(String[] args) {
         long cputime;
         TMB = ManagementFactory.getThreadMXBean();
@@ -11,22 +12,42 @@ class TestBst {
             System.exit(0);
         }
 
-        String fileName = "1000words.txt";
-        String keyFileName = "2000words.txt";
-        BST bst = new BST();
-        buildBST(bst, "./program2/public/"+fileName);
+        String path =  "./program2/public/";//"";//
+        String fileName = "10words.txt";
+        String keyFileName = "10words.txt";
+
+        // construct bsts
+        BST nobst = new BST();
+        buildBST(nobst, path + fileName);
+        BST obst = new BST();
+        buildBST(obst, path + fileName);
         AVL avl = new AVL();
-        buildBST(avl, "./program2/public/"+fileName);
-        System.out.println(
-                "Number of words in the BST: " + bst.size() + " (number of insertions: " + bst.sumFreq() + ")");
+        buildBST(avl, path + fileName);
 
-        System.out.println("Sum of Weighted Path Lengths (BST): " + bst.sumWeightedPath());
-        bst.resetCounters();
-        probeBST(bst, "./program2/public/"+fileName);
-
+        // (2) AVL test
         System.out.println("Sum of Weighted Path Lengths (AVL): " + avl.sumWeightedPath());
         avl.resetCounters();
-        probeBST(avl, "./program2/public/"+keyFileName);
+        probeBST(avl, path + keyFileName);
+
+        // (3) Transform another plain BST into an NOBST and repeat probing.
+        cputime = TMB.getCurrentThreadCpuTime();
+        nobst.nobst();
+        cputime = TMB.getCurrentThreadCpuTime() - cputime;
+        System.out.println("CPU time to convert to an NOBST: " + (cputime / 1000000) + " millisec");
+        System.out.println("Sum of Weighted Path Lengths (NOBST): " + nobst.sumWeightedPath());
+
+        nobst.resetCounters();
+        probeBST(nobst, path + keyFileName);
+
+        // (3) Transform another plain BST into an NOBST and repeat probing.
+        cputime = TMB.getCurrentThreadCpuTime();
+        obst.obst();
+        cputime = TMB.getCurrentThreadCpuTime() - cputime;
+        System.out.println("CPU time to convert to an OBST: " + (cputime / 1000000) + " millisec");
+        System.out.println("Sum of Weighted Path Lengths (OBST): " + obst.sumWeightedPath());
+
+        obst.resetCounters();
+        probeBST(obst, path + keyFileName);
     }
 
     public static void buildBST(BST bst, String input) {
@@ -37,7 +58,7 @@ class TestBst {
             bst.insert(sfs.readWord());
         cputime = TMB.getCurrentThreadCpuTime() - cputime;
 
-        //bst.print();
+        // bst.print();
         String bstType = (bst instanceof AVL) ? "AVL" : "BST";
         System.out.println("CPU time to build a(n) " + bstType + ": " + (cputime / 1000000) + " millisec");
     }
@@ -56,7 +77,7 @@ class TestBst {
         }
         cputime = TMB.getCurrentThreadCpuTime() - cputime;
 
-        //bst.print();
+        // bst.print();
         String bstType = "BST";
         if (bst instanceof AVL)
             bstType = "AVL";
